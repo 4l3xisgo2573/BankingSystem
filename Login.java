107dd3c5b197ee25535a1ebc47602317ed6578aa
4l3xisgo2573 <110707335+4l3xisgo2573@users.noreply.github.com>
2023-10-29 19:52:44 -0500

    Making changes

--- Login.java
+++ Login.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Login {
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter username: ");
+        String username = scan.nextLine();
+        System.out.println("Enter password: ");
+        String password = scan.nextLine();
+        scan.close();
+        System.out.println(username);
+        System.out.println(password);
         System.out.println("Hello World");
     }
 }
